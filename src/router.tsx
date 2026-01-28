9e2463c1892047d5b75633d0b634adaded98902a
Karthik G. Appiah <karthikgappiah@gmail.com>
2026-01-27 23:10:38 -0800

    config: created basic templating (ref #2)

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+// --------------------------- [ Router Configuration ] ---------------------------
+// [ Location ]: ~/src/router.tsx
+// [ Purpose  ]: To configure the routing behavior of TanStack Router.
+// --------------------------------------------------------------------------------
+
+import { createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+export function getRouter() {
+  const router = createRouter({
+    routeTree,
+    scrollRestoration: true,
+  })
+
+  return router
+}
---
+++ src/routes/__root.tsx
@@ -0,0 +1,53 @@
+// ------------------------------- [ Root Layout ] --------------------------------
+// [ Location ]: ~/src/routes/__root.tsx
+// [ Purpose  ]: To wrap every other route in this application.
+// --------------------------------------------------------------------------------
+
+/// <reference types="vite/client" />
+import type { ReactNode } from 'react'
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from '@tanstack/react-router'
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+  }),
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  )
+}
