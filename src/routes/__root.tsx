03169443d26bc192bbddf789c44e2f15acb718a9
Karthik G. Appiah <karthikgappiah@gmail.com>
2026-01-29 02:52:04 +0000

    chore: added lang tag to html element (ref #4)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -44,7 +44,7 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <HeadContent />
       </head>
