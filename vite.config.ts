99b056b038e3099e0216c2a4ccb5453f1d3b0692
Karthik G. Appiah <karthikgappiah@gmail.com>
2026-01-29 04:03:23 +0000

    config: added nitro plugin to deploy vite (ref #11)

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -17,6 +18,7 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tanstackStart(),
+    nitro(),
     viteReact(), // WARNING: tanstackStart must precede viteReact.
     tailwindcss(),
   ],
